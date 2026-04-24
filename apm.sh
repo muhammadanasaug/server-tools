8c71ed9713f5e9414197cf2070729daeeb04ff97
Muhammad Anas <muhammadanasaug@gmail.com>
2026-04-24 05:37:27 +0000

    auto-run service reset on execution

--- apm.sh
+++ apm.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-reset-services () {
-    local services=("nginx" "varnish" "apache2" "php-fpm" "mysql" "memcached" "redis-server" "imunify360")
+services=("nginx" "varnish" "apache2" "php-fpm" "mysql" "memcached" "redis-server" "imunify360")
 
-    for s in "${services[@]}"; do
-        [[ $s == "php-fpm" ]] && s=$(php -v | awk '{print "php"substr($2,1,3)"-fpm";exit}')
-        [[ $s == "imunify360" ]] && s=$(systemctl list-unit-files | grep -q imunify360-agent && echo imunify360-agent || echo imunify360)
+for s in "${services[@]}"; do
+    [[ $s == "php-fpm" ]] && s=$(php -v | awk '{print "php"substr($2,1,3)"-fpm";exit}')
+    [[ $s == "imunify360" ]] && s=$(systemctl list-unit-files | grep -q imunify360-agent && echo imunify360-agent || echo imunify360)
 
-        if systemctl restart "$s" &>/dev/null; then
-            echo "$(tput setaf 2)   |✓ $s restarted$(tput sgr0)"
-        else
-            echo "$(tput setaf 1)   |✗ $s failed$(tput sgr0)"
-        fi
-    done
+    if systemctl restart "$s" &>/dev/null; then
+        echo "$(tput setaf 2)   |✓ $s restarted$(tput sgr0)"
+    else
+        echo "$(tput setaf 1)   |✗ $s failed$(tput sgr0)"
+    fi
+done
 
-    swapoff -a && swapon -a && \
-        echo "$(tput setaf 2)   |✓ Swap cleared$(tput sgr0)" || \
-        echo "$(tput setaf 1)   |✗ Swap clear failed$(tput sgr0)"
-}
+swapoff -a && swapon -a && \
+    echo "$(tput setaf 2)   |✓ Swap cleared$(tput sgr0)" || \
+    echo "$(tput setaf 1)   |✗ Swap clear failed$(tput sgr0)"
